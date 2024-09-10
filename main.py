3cb388a23958b7503e024c045aef18576e9d71c4
nik-kubala <nikolas.kubala81@gmail.com>
2024-09-10 09:30:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("cau")
+x = 10
+x = x + 2   # x+=2
+x = x * 2   # x*=2
+
+y = x % 2      # % je modulo a to je zvyšok po delení
+z = x // 3     # // je delenie bezozvyšku
+
+# trik
+124 % 10 = 2    # posledna cifra
+124 // 10 = 12  # prvé dve cifry
+
+if x % 2 == 0:  # či je číslo párne
+    x % 2 != 0: # != znamená nerovná sa, toto je pre nepárne alebo
+    x % 2 == 1:
+
+** je umocnenie
+
+
+print(x)
